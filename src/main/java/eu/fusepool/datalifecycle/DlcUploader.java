2b0bbad8671258f2526fd9c9a65f0f9468e68ff0
Luigi Selmi <selmi_luigi@hotmail.com>
2014-02-04 14:45:33 +0100

    added method for multipart messages

--- src/main/java/eu/fusepool/datalifecycle/DlcUploader.java
+++ src/main/java/eu/fusepool/datalifecycle/DlcUploader.java
@@ -1,16 +1,11 @@
 package eu.fusepool.datalifecycle;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.security.AccessController;
 import java.security.AllPermission;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
@@ -18,26 +13,25 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.access.TcManager;
 import org.apache.clerezza.rdf.core.impl.SimpleMGraph;
 import org.apache.clerezza.rdf.core.serializedform.Parser;
-import org.apache.clerezza.rdf.core.serializedform.Serializer;
 import org.apache.clerezza.rdf.core.serializedform.SupportedFormat;
-import org.apache.commons.io.IOUtils;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
-import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.sun.jersey.multipart.FormDataParam;
+
 
 
 
@@ -83,6 +77,30 @@ public class DlcUploader {
     		@HeaderParam("Content-Type") String mediaType,
     		@HeaderParam("Dataset") String dataset,
             final InputStream stream) throws Exception {
+    	
+        AccessController.checkPermission(new AllPermission());
+        final MGraph graph = new SimpleMGraph();
+        
+        String message = "";
+       
+        if(mediaType.equals(SupportedFormat.RDF_XML)) {
+        	parser.parse(graph, stream, SupportedFormat.RDF_XML);
+        }
+        else {
+        	message = "Add header Content-Type: application/rdf+xml ";
+        }
+        
+        return message + "Added " + graph.size() + " triples  to dataset " + dataset + "\n";
+    }
+    
+    @POST
+    @Path("multipartrdf")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces("text/plain")
+    public String uploadMultipartRdf(@Context final UriInfo uriInfo,  
+    		@HeaderParam("Content-Type") String mediaType,
+    		@FormDataParam("data") InputStream stream,
+    		@FormDataParam("dataset") String dataset) throws Exception {
     	
         AccessController.checkPermission(new AllPermission());
         final MGraph graph = new SimpleMGraph();
