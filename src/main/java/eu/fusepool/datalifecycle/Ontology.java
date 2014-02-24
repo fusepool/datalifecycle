5ec76d6ba5afdc378d0e4a5bcc68d8411ca39dfe
Luigi Selmi <selmi_luigi@hotmail.com>
2014-02-24 17:19:20 +0100

    issue #6 added rdfize and enhance service properties to the application ontology

--- src/main/java/eu/fusepool/datalifecycle/Ontology.java
+++ src/main/java/eu/fusepool/datalifecycle/Ontology.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2013 Fusepool Project.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package eu.fusepool.datalifecycle;
 
 import org.apache.clerezza.rdf.core.UriRef;
@@ -34,12 +50,17 @@ public class Ontology {
     // Properties
     
     public static final UriRef pipe = new UriRef("http://example.com/ont/workflow/pipe");
+    
     // a pipe creates one or more tasks
     public static final UriRef creates = new UriRef("http://example.com/ont/workflow/creates");
     // a task delivers a product 
     public static final UriRef deliverable = new UriRef("http://example.com/ont/workflow/deliverable");
     // a task delivers a product using a service
     public static final UriRef service = new UriRef("http://example.com/ont/workflow/service");
+    // a service sub property for xml to rdf transformation
+    public static final UriRef rdfizeService = new UriRef("http://example.com/ont/workflow/rdfizeService");
+    // a service sub property for data enhancements
+    public static final UriRef enhanceService = new UriRef("http://example.com/ont/workflow/enhanceService");
     
     // VOID ontology
     // Dataset
